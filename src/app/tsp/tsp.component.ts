1f2c69aefcba9f1263da517486a700a53af4164f
Luis H. P. Mendes <luishpmendes@gmail.com>
2023-07-11 11:39:44 -0300

    Resizing svg
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- src/app/tsp/tsp.component.ts
+++ src/app/tsp/tsp.component.ts
@@ -43,27 +43,20 @@ export class TSPComponent implements OnInit, AfterViewInit {
     const data = this.locations.data.map(location => ({ id: location.id, x: location.x, y: location.y }));
 
     const svg = d3.select(element).append('svg')
-      .attr('width', '1000')
-      .attr('height', '1000');
+      .attr('width', '500')
+      .attr('height', '500');
 
     const x = d3.scaleLinear()
       .domain([0, 1000])
-      .range([0, 1000]);
+      .range([0, 500]);
 
     const y = d3.scaleLinear()
       .domain([0, 1000])
-      .range([0, 1000]);
+      .range([0, 500]);
 
     const line = d3.line<any>()
       .x(d => x(d.x))
       .y(d => y(d.y));
-
-    // svg.append('path')
-    //   .datum(data)
-    //   .attr('fill', 'none')
-    //   .attr('stroke', 'steelblue')
-    //   .attr('stroke-width', 1.5)
-    //   .attr('d', line);
 
     svg.selectAll('.dot')
       .data(data)
@@ -87,41 +80,52 @@ export class TSPComponent implements OnInit, AfterViewInit {
 
   tsp(timeLimit: string, maxGen : string, p : string, pe : string, pm : string, rho : string) : void {
     console.log("TspComponent tsp")
+
     if (Number(timeLimit) == 0 && Number(maxGen) == 0) {
       alert("Either time limit or max generations must be greater than 0");
       return;
     }
+
     let result = this.tspService.solve(this.locations.data, Number(timeLimit), Number(maxGen), Number(p), Number(pe), Number(pm), Number(rho));
     let solution = result[1];
     solution.push(solution[0]);
+
     const element = this.chartContainer.nativeElement;
     const data = solution.map(location => ({ id: location.id, x: location.x, y: location.y }));
+
     d3.select(element).select('svg').remove();
+
     const svg = d3.select(element).append('svg')
-      .attr('width', '1000')
-      .attr('height', '1000');
+      .attr('width', '500')
+      .attr('height', '500');
+
     const x = d3.scaleLinear()
       .domain([0, 1000])
-      .range([0, 1000]);
+      .range([0, 500]);
+
     const y = d3.scaleLinear()
       .domain([0, 1000])
-      .range([0, 1000]);
+      .range([0, 500]);
+
     const line = d3.line<any>()
       .x(d => x(d.x))
       .y(d => y(d.y));
+
     svg.append('path')
       .datum(data)
       .attr('fill', 'none')
       .attr('stroke', 'steelblue')
       .attr('stroke-width', 1.5)
       .attr('d', line);
-      svg.selectAll('.dot')
+
+    svg.selectAll('.dot')
       .data(data)
       .enter().append('circle')
       .attr('class', 'dot')
       .attr('cx', d => x(d.x))
       .attr('cy', d => y(d.y))
       .attr('r', 5);
+
     // Add labels
     svg.selectAll('.text')
       .data(data)
